57e2a74e070972ae4addc392386cee1564e502bf
Daniel P. Shannon <daniel@try.works>
2023-12-08 16:43:06 -0500

    🌱 Seed Neo4j with complete interviews.

--- packages/scripts/src/aws-to-neo4j.ts
+++ packages/scripts/src/aws-to-neo4j.ts
@@ -12,17 +12,15 @@ import neo4j from 'neo4j-driver'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-async function main () {
-    const data = JSON.parse(
-        await fs.readFile(
-            path.join(__dirname, '../assets/035_larry_kramer.json'),
-            'utf8',
-        ),
-    )
+const driver = neo4j.driver(
+    'bolt://localhost:7687',
+    neo4j.auth.basic('neo4j', 'auohpauohp'),
+)
 
+async function seed ({ data, date, interviewee, interviewNumber }: any) {
     if (
         !Array.isArray(data?.results?.speaker_labels?.segments) ||
-    data.results.speaker_labels.segments.length === 0
+        data.results.speaker_labels.segments.length === 0
     ) {
         throw new Error('No speaker labels found')
     }
@@ -43,54 +41,125 @@ async function main () {
         }
     })
 
-    await fs.writeFile(
-        path.join(__dirname, '../assets/035_larry_kramer_segments.json'),
-        JSON.stringify(segments, null, 2),
-    )
+    await driver.executeQuery(`
+        CREATE (i:Interview {number: $interviewNumber, date: date($date)})
+        MERGE (interviewee:Person {name: $interviewee})
+        MERGE (jim:Person {name: 'Jim Hubbard'})
+        MERGE (sarah:Person {name: 'Sarah Schulman'})
+        CREATE (sarahSpeaker:Speaker {remoteID: 'spk_2'})
+        CREATE (jimSpeaker:Speaker {remoteID: 'spk_1'})
+        CREATE (intervieweeSpeaker:Speaker {remoteID: 'spk_1'})
+        MERGE (interviewee)-[:INTERVIEWED_AS]->(intervieweeSpeaker)
+        MERGE (jim)-[:INTERVIEWED_AS]->(jimSpeaker)
+        MERGE (sarah)-[:INTERVIEWED_AS]->(sarahSpeaker)
+        CREATE (i)-[:INTERVIEWED_WITH]->(intervieweeSpeaker)
+        CREATE (i)-[:INTERVIEWED_WITH]->(jimSpeaker)
+        CREATE (i)-[:INTERVIEWED_WITH]->(sarahSpeaker)
+    `, {
+        date,
+        interviewee,
+        interviewNumber,
+    })
 
-    const driver = neo4j.driver(
-        'bolt://localhost:7687',
-        neo4j.auth.basic('neo4j', 'auohpauohp'),
-    )
+    for await (let segment of segments) {
+        await driver.executeQuery(`
+            MATCH (speaker:Speaker {remoteID: $speakerID}) <-[:INTERVIEWED_WITH]- (interview:Interview {number: $interviewNumber})
+            CREATE (s:Statement {text: $text})
+            MERGE (speaker)-[:SAYS {startTime: $startTime, endTime: $endTime}]->(s)
+        `, {
+            text: segment.text,
+            speakerID: segment.speaker,
+            startTime: segment.startTime,
+            endTime: segment.endTime,
+            interviewNumber,
+        })
+    }
+}
 
+async function main () {
     await driver.getServerInfo()
 
     await driver.executeQuery(`
-    MATCH p=()--()
-    DETACH DELETE p
-  `)
-    await driver.executeQuery(`
-    CREATE (i:Interview {number: 35, date: date('2003-11-15')})
-    CREATE (larry:Person {name: 'Larry Kramer'})
-    CREATE (sarah:Person {name: 'Sarah Schulman'})
-    CREATE (jim:Person {name: 'Jim Hubbard'})
-    CREATE (jimSpeaker:Speaker {remoteID: "spk_0"})
-    CREATE (larrySpeaker:Speaker {remoteID: "spk_2"})
-    CREATE (sarahSpeaker:Speaker {remoteID: "spk_1"})
-    CREATE (larry)-[:INTERVIEWED_AS]->(larrySpeaker)
-    CREATE (sarah)-[:INTERVIEWED_AS]->(sarahSpeaker)
-    CREATE (jim)-[:INTERVIEWED_AS]->(jimSpeaker)
-    CREATE (i)-[:INTERVIEWED_WITH]->(larrySpeaker)
-    CREATE (i)-[:INTERVIEWED_WITH]->(sarahSpeaker)
-    CREATE (i)-[:INTERVIEWED_WITH]->(jimSpeaker)
-  `)
+        MATCH p=()--()
+        DETACH DELETE p
+    `)
 
-    for await (const segment of segments) {
-        await driver.executeQuery(
-            `
-      CREATE (s:Statement {text: $text})
-      WITH s
-      MATCH (speaker:Speaker {remoteID: $speakerID})
-      MERGE (speaker)-[:SAYS {startTime: $startTime, endTime: $endTime}]->(s)
-    `,
-            {
-                text: segment.text,
-                speakerID: segment.speaker,
-                startTime: segment.startTime,
-                endTime: segment.endTime,
-            },
-        )
-    }
+    let data
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~ 004 - Gregg Bordowitz ~~~~~~~~~~~~~~~~~~~~~~~~~ //
+    data = JSON.parse(
+        await fs.readFile(
+            path.join(__dirname, '../assets/004_gregg_bordowitz.json'),
+            'utf8',
+        ),
+    )
+    await seed({
+        data,
+        date: '2020-05-27',
+        interviewee: 'Gregg Bordowitz',
+        interviewNumber: 4,
+    })
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~ 012 - Mark Harrington ~~~~~~~~~~~~~~~~~~~~~~~~~ //
+    data = JSON.parse(
+        await fs.readFile(
+            path.join(__dirname, '../assets/012_mark_harrington.json'),
+            'utf8',
+        ),
+    )
+    await seed({
+        data,
+        date: '2020-05-27',
+        interviewee: 'Mark Harrington',
+        interviewNumber: 12,
+    })
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~ 035 - Larry Kramer ~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+    data = JSON.parse(
+        await fs.readFile(
+            path.join(__dirname, '../assets/035_larry_kramer.json'),
+            'utf8',
+        ),
+    )
+    await seed({
+        data,
+        date: '2020-05-27',
+        interviewee: 'Larry Kramer',
+        interviewNumber: 35,
+    })
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~ 074 - Douglas Crimp ~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+    data = JSON.parse(
+        await fs.readFile(
+            path.join(__dirname, '../assets/074_douglas_crimp.json'),
+            'utf8',
+        ),
+    )
+    await seed({
+        data,
+        date: '2020-05-27',
+        interviewee: 'Douglas Crimp',
+        interviewNumber: 74,
+    })
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 138 - Joan Gibbs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+    data = JSON.parse(
+        await fs.readFile(
+            path.join(__dirname, '../assets/138_joan_gibbs.json'),
+            'utf8',
+        ),
+    )
+    await seed({
+        data,
+        date: '2020-05-27',
+        interviewee: 'Joan Gibbs',
+        interviewNumber: 138,
+    })
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
     await driver.close()
 }
