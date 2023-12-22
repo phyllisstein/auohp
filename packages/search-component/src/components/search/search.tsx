0c663a9575e76ca1567a1fb069b288b35f0ea694
Daniel P. Shannon <daniel@try.works>
2023-12-22 03:23:01 -0500

    ✨ Search Neo4j from browser.

--- packages/search-component/src/components/search/search.tsx
+++ packages/search-component/src/components/search/search.tsx
@@ -1,16 +1,32 @@
+import queryString from 'query-string'
 import { useState, useTransition } from 'react'
 
-import { useNeo4jSearch } from './use-neo4j-search'
+import { SearchResult, useNeo4jSearch } from './use-neo4j-search'
 
 export function Search () {
     const [search, setSearch] = useState<string>('')
-    const [results, setResults] = useState<string[]>([])
     const [_, startTransition] = useTransition()
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         startTransition(() => {
             setSearch(e.target.value)
         })
+    }
+
+    const handleResultClick = (result: SearchResult) => {
+        // window.history.pushState(null, '', queryString.stringifyUrl({
+        //     url: window.location.href,
+        //     query: {
+        //         ...queryString.parse(location.search),
+        //         timestamp: result.startTime,
+        //     },
+        // }))
+        const hash = queryString.stringify({
+            ...queryString.parse(location.hash),
+            timestamp: result.startTime,
+        })
+
+        window.location.hash = hash
     }
 
     const searchResults = useNeo4jSearch(search, 'transcriptSearch')
@@ -21,9 +37,10 @@ export function Search () {
             <div>
                 {
                     searchResults.map(result => (
-                        <div key={ result.id }>
+                        <div key={ result.uuid } onClick={ () => handleResultClick(result) }>
                             <h3>{ result.statement }</h3>
                             <p>{ result.speaker }</p>
+                            <aside>{ result.startTime }</aside>
                         </div>
                     ))
                 }
