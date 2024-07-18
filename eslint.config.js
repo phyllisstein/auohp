2218983cc171f06bcb25ce345634888b7769ebec
Daniel P. Shannon <daniel@try.works>
2024-07-18 08:06:22 -0400

    🚨 Don't sort keys.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,7 @@
 import stylistic from '@stylistic/eslint-plugin'
 import parserTS from '@typescript-eslint/parser'
-import tsSortKeys from 'eslint-plugin-typescript-sort-keys'
 import tseslint from 'typescript-eslint'
 import typescriptESLint from '@typescript-eslint/eslint-plugin'
-import sortDestructureKeys from 'eslint-plugin-sort-destructure-keys'
-import sortKeysFix from 'eslint-plugin-sort-keys-fix'
 import eslint from '@eslint/js'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import react from 'eslint-plugin-react'
@@ -28,10 +25,15 @@ export default [
             '@stylistic': stylistic,
             'jsx-a11y': jsxA11y,
             react,
-            'sort-destructure-keys': sortDestructureKeys,
-            'sort-keys-fix': sortKeysFix,
         },
         rules: {
+            '@stylistic/arrow-parens': [
+                'warn',
+                'as-needed',
+                {
+                    requireForBlockBody: false,
+                },
+            ],
             '@stylistic/brace-style': [
                 'warn',
                 '1tbs',
@@ -61,13 +63,6 @@ export default [
                         'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
                         'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key',
                     ],
-                },
-            ],
-            '@stylistic/arrow-parens': [
-                'warn',
-                'as-needed',
-                {
-                    requireForBlockBody: false,
                 },
             ],
             '@stylistic/jsx-closing-bracket-location': ['warn', 'after-props'],
@@ -103,7 +98,7 @@ export default [
                 {
                     callbacksLast: true,
                     ignoreCase: true,
-                    noSortAlphabetically: false,
+                    noSortAlphabetically: true,
                     reservedFirst: true,
                     shorthandFirst: true,
                 },
@@ -161,20 +156,6 @@ export default [
             ],
             '@stylistic/space-before-function-paren': 'warn',
             '@stylistic/template-curly-spacing': ['warn', 'always'],
-            'sort-destructure-keys/sort-destructure-keys': [
-                'warn',
-                {
-                    caseSensitive: false,
-                },
-            ],
-            'sort-keys-fix/sort-keys-fix': [
-                'warn',
-                'asc',
-                {
-                    caseSensitive: false,
-                    natural: true,
-                },
-            ],
         },
     },
     {
@@ -192,7 +173,6 @@ export default [
         },
         plugins: {
             '@typescript-eslint': typescriptESLint,
-            'typescript-sort-keys': tsSortKeys,
         },
         rules: {
             ...tseslint.configs.recommendedTypeChecked.rules,
@@ -209,8 +189,6 @@ export default [
             ],
             'no-undef': 'off',
             'no-unused-vars': 'off',
-            'typescript-sort-keys/interface': 'warn',
-            'typescript-sort-keys/string-enum': 'warn',
         },
     },
 ]
