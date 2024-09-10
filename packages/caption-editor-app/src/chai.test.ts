166dd5be77bcc31479c9e6c8f5ab4f9f2cb487a4
Daniel P. Shannon <daniel@try.works>
2024-09-10 15:20:25 -0400

    ✅ Revel in Chai prototype-mutating glory.

---
+++ packages/caption-editor-app/src/chai.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest'
+
+const SOME_STRING = 'some string'
+
+describe('expect (best practice)', () => {
+    describe('is so ugly', () => {
+        it('should assert that a string is equal to another', () => {
+            expect(SOME_STRING).toEqual('some string')
+        })
+
+        it('should assert that a string is not equal to another', () => {
+            expect(SOME_STRING).not.toEqual('another string')
+        })
+    })
+})
+
+
+describe('chai (YOOPOO: You Only Overwrite Prototype of Object Once)', () => {
+    describe('is so beautiful', () => {
+        it('should assert that a string is equal to another', () => {
+            SOME_STRING.should.equal('some string')
+        })
+
+        it('should assert that a string is not equal to another', () => {
+            SOME_STRING.should.not.equal('another string')
+        })
+    })
+})
