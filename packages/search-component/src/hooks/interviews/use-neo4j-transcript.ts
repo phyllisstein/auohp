1036bc48a4182f3da10ff9d61746a56135c15304
Daniel P. Shannon <daniel@try.works>
2024-09-23 07:09:05 -0400

    👽️ Align search components with new schema.

--- packages/search-component/src/hooks/interviews/use-neo4j-transcript.ts
+++ packages/search-component/src/hooks/interviews/use-neo4j-transcript.ts
@@ -2,18 +2,41 @@ import { useEffect, useState } from 'react'
 
 import { useNeo4j } from 'hooks/infrastructure'
 
+export interface Person {
+    name: string
+    uid: string
+}
+
+export interface Statement {
+    text: string
+}
+
+export interface Interview {
+    number: number
+    uid: string
+}
+
+export interface Video {
+    url: string
+}
+
 export interface Neo4jResult {
-    interviewNumber: number
-    interviewURL: string
-    score: number
-    speaker: string
-    statement: string
-    timestamp: number
-    videoURL: string
+    person: Person
+    speakerSays: SaysEdge
+    statement: Statement
+    interview: Interview
+    video: Video
+}
+
+interface SaysEdge {
+    startTimestamp: string
+    endTimestamp: string
+    startTime: number
+    endTime: number
 }
 
 export function useNeo4jTranscript(query: string, index: string): Neo4jResult[] {
-    const driver = useNeo4j('bolt://localhost:7687', 'neo4j', 'auohpauohp')
+    const driver = useNeo4j('bolt+s://bolt.auohp.here:443', 'neo4j', 'auohpauohp')
     const [searchResults, setSearchResults] = useState<Neo4jResult[]>([])
 
     useEffect(() => {
@@ -28,24 +51,17 @@ export function useNeo4jTranscript(query: string, index: string): Neo4jResult[] 
                 `
                     CALL db.index.fulltext.queryNodes($index, $query) YIELD node AS statement, score
                     MATCH (statement)<-[speakerSays:SAYS]-(speaker:Interviewee)<-[:INTERVIEWED_AS]-(person)
-                    MATCH (speaker)<-[:INTERVIEWED_WITH]-(interview)-[:HAS_VIDEO]->(video)
-                    RETURN speakerSays.startTime AS timestamp,
-                        person.name AS speaker,
-                        statement.text AS statement,
-                        interview.number AS interviewNumber,
-                        interview.url AS interviewURL,
-                        video.url AS videoURL,
-                        score
+                    MATCH (speaker)<-[:INCLUDES_SPEAKER]-(transcript)<-[:HAS_TRANSCRIPT]-(video)<-[:HAS_VIDEO]-(interview)
+                    RETURN statement, person, speakerSays, interview, video
                     ORDER BY score DESC
                 `, { index, query })
 
             const searchResults = result.records.map(record => ({
-                interviewNumber: record.get('interviewNumber'),
-                score: record.get('score'),
-                speaker: record.get('speaker'),
-                statement: record.get('statement'),
-                timestamp: record.get('timestamp'),
-                videoURL: record.get('videoURL'),
+                person: record.get('person').properties,
+                speakerSays: record.get('speakerSays').properties,
+                statement: record.get('statement').properties,
+                interview: record.get('interview').properties,
+                video: record.get('video').properties,
             }))
 
             setSearchResults(searchResults)
