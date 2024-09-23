aec2c7790a7e5e6a96fe46faefaaceb974ebbc83
Daniel P. Shannon <daniel@try.works>
2024-09-23 09:26:12 -0400

    ⚡️ Manipulate transcript JSON on server.

--- packages/caption-editor-app/src/app/api/transcript.json/route.ts
+++ packages/caption-editor-app/src/app/api/transcript.json/route.ts
@@ -7,14 +7,17 @@ const __dirname = path.dirname(__filename)
 
 
 export async function GET() {
-    let json = await fs.readFile(path.resolve(__dirname, '../../../../public/assets/demo/025_lei_chou.captions.json'), 'utf-8')
-    json = JSON.parse(json)
-    json = json.sort((a, b) => b.startTime - a.startTime)
-    json = JSON.stringify(json)
+    const jsonString = await fs.readFile(path.resolve(__dirname, '../../../../public/assets/demo/025_lei_chou.captions.json'), 'utf-8')
+    let json = JSON.parse(jsonString)
+    json = [{
+        type: 'transcript',
+        children: json,
+    }]
 
-    return new Response(json, {
+    return Response.json(json, {
         headers: {
             'Content-Type': 'application/json',
         },
     })
 }
+
