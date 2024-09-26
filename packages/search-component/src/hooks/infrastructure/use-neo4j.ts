eaa4fe16586a095672699ade869ab54eeb425feb
Daniel P. Shannon <daniel@try.works>
2024-09-26 02:04:30 -0400

    Final Docker tweaks.

--- packages/search-component/src/hooks/infrastructure/use-neo4j.ts
+++ packages/search-component/src/hooks/infrastructure/use-neo4j.ts
@@ -2,9 +2,9 @@ import neo4j, {type Driver} from 'neo4j-driver'
 import {useEffect, useState} from 'react'
 
 const {
-  NEO4J_URI = 'neo4j://localhost:7687',
+  NEO4J_PASSWORD = 'UmjT5CRbO2bZrL2-3ubNCilKtYOXudX5hHL8akj4s5Y',
+  NEO4J_URI = 'neo4j+s://0faeaca6.databases.neo4j.io',
   NEO4J_USERNAME = 'neo4j',
-  NEO4J_PASSWORD = 'auohpauohp',
 } = import.meta.env
 
 export function useNeo4j(
