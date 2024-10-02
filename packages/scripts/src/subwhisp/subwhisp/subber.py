739ee7649ed0da806117549b98e806ea35375f89
Daniel P. Shannon <daniel@try.works>
2024-10-02 08:51:25 -0400

    Typo

--- packages/scripts/src/subwhisp/subwhisp/subber.py
+++ packages/scripts/src/subwhisp/subwhisp/subber.py
@@ -45,7 +45,7 @@ def get_time_span(span: Span, timing: dict):
     end_token = span[-1]
     while (start_token.is_punct or not timing.get(start_token.idx, None)) and start_token.i > 0:
         start_token = start_token.nbor(-1)
-    while (end_token.is_punct or not timing.get(end_token.idx, None)) and end_token.i < 0:
+    while (end_token.is_punct or not timing.get(end_token.idx, None)) and end_token.i > 0:
         end_token = end_token.nbor(-1)
     end_index = end_token.idx
     start_index = start_token.idx
