e45ffc0dacd320ec954f31b5af23cbb466fd0957
Daniel P. Shannon <daniel@try.works>
2024-10-05 14:48:39 -0400

    Bundle up some config tweaks.

--- packages/search-component/src/hooks/infrastructure/use-neo4j.ts
+++ packages/search-component/src/hooks/infrastructure/use-neo4j.ts
@@ -1,9 +1,16 @@
 import neo4j, {type Driver} from 'neo4j-driver'
 import {useEffect, useState} from 'react'
 
+// const {
+//   NEO4J_PASSWORD = 'UmjT5CRbO2bZrL2-3ubNCilKtYOXudX5hHL8akj4s5Y',
+//   NEO4J_URI = 'neo4j+s://0faeaca6.databases.neo4j.io',
+//   NEO4J_USERNAME = 'neo4j',
+// } = import.meta.env
+//
+
 const {
-  NEO4J_PASSWORD = 'UmjT5CRbO2bZrL2-3ubNCilKtYOXudX5hHL8akj4s5Y',
-  NEO4J_URI = 'neo4j+s://0faeaca6.databases.neo4j.io',
+  NEO4J_PASSWORD = 'auohpauohp',
+  NEO4J_URI = 'bolt+s://bolt.auohp.here:443',
   NEO4J_USERNAME = 'neo4j',
 } = import.meta.env
 
