b1c8de077b4e9d80209694645ce69a5db2ba18c7
Daniel P. Shannon <daniel@try.works>
2024-10-05 22:29:04 -0400

    Capture last-viewed timestamp.

--- packages/search-component/src/components/player/player.tsx
+++ packages/search-component/src/components/player/player.tsx
@@ -12,38 +12,58 @@ interface PlayerProps {
 export function Player({url}: PlayerProps) {
   const player = useRef<HTMLVideoElement>(null)
   const video = useNeo4jVideo(url)
+  const hasSetTimestamp = useRef<boolean>(false)
 
   useEffect(() => {
     const currentPlayer = player.current
 
     const handler = () => {
-      if (typeof window === 'undefined' || !currentPlayer) {
+      if (typeof window === 'undefined' || !currentPlayer || hasSetTimestamp.current) {
         return
       }
 
-      const parsedURL = queryString.parseUrl(window.location.href, {parseNumbers: true, sort: false})
-      if (typeof parsedURL.query.timestamp === 'undefined') {
-        return
+      const parsedURL = queryString.parseUrl(window.location.href)
+      const localStorageTimestamp = localStorage.getItem('last-timestamp')
+
+      currentPlayer.currentTime
+        = typeof parsedURL.query.timestamp === 'string'
+          ? Number.parseFloat(parsedURL.query.timestamp)
+          : localStorageTimestamp !== null
+            ? Number.parseFloat(localStorageTimestamp)
+            : 0
+
+      const strURL = queryString.stringifyUrl(parsedURL)
+      const strippedURL = queryString.exclude(strURL, ['timestamp'])
+      if (window.location.href !== strippedURL) {
+        hasSetTimestamp.current = true
+        window.history.replaceState('', document.title, strippedURL)
       }
-
-      currentPlayer.currentTime = parsedURL.query.timestamp as unknown as number
-
-      let strippedURL = queryString.stringifyUrl(parsedURL)
-      strippedURL = queryString.exclude(strippedURL, ['timestamp'])
-      window.history.pushState('', document.title, strippedURL)
     }
 
     void handler()
+  }, [player])
 
-    const hashAbortController = new AbortController()
-    window.addEventListener('hashchange', handler, {
-      signal: hashAbortController.signal,
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+
+    const currentPlayer = player.current
+
+    const handleBeforeUnload = () => {
+      if (currentPlayer) {
+        const currentTime = currentPlayer.currentTime
+        localStorage.setItem('last-timestamp', currentTime.toString())
+      }
+    }
+
+    const unloadAbortController = new AbortController()
+    window.addEventListener('beforeunload', handleBeforeUnload, {
+      signal: unloadAbortController.signal,
     })
 
     return () => {
-      hashAbortController.abort()
+      unloadAbortController.abort()
     }
-  })
+  }, [player])
 
   return (
     <div className='player-container'>
