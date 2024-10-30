8805af4b2dbe3664fba4802c7958542795fd8794
Daniel P. Shannon <daniel@try.works>
2024-10-30 03:17:18 -0400

    Configure vector search.

--- packages/search-component/src/hooks/interviews/use-neo4j-transcript.ts
+++ packages/search-component/src/hooks/interviews/use-neo4j-transcript.ts
@@ -2,12 +2,22 @@ import {useEffect, useState} from 'react'
 import {useNeo4j} from 'hooks/infrastructure'
 import type {Neo4jResult} from './types'
 
-export function useNeo4jTranscript(query: string, index: string): Neo4jResult[] {
+
+const {
+  VITE_OPENAI_API_KEY: OPENAI_API_KEY,
+} = import.meta.env
+
+
+export function useNeo4jTranscript(query: string): Neo4jResult[] {
   const driver = useNeo4j()
   const [searchResults, setSearchResults] = useState<Neo4jResult[]>([])
 
   useEffect(() => {
-    if (!driver || !index || !query) {
+    if (!OPENAI_API_KEY) {
+      throw new Error('Must provide an OpenAI API key')
+    }
+
+    if (!driver || !query) {
       setSearchResults([])
       return
     }
@@ -16,14 +26,14 @@ export function useNeo4jTranscript(query: string, index: string): Neo4jResult[] 
       const result = await driver.executeQuery(
         // language=Cypher
         `
-          CALL db.index.fulltext.queryNodes($index, $query) YIELD node AS statement, score
+          CALL genai.vector.encodeBatch([$query], 'OpenAI', {token: $token}) YIELD vector
+          CALL db.index.vector.queryNodes('statement_embedding', 15, vector) YIELD node AS statement
           MATCH (statement)<-[meta:TRANSCRIBES]-(transcript) <-[:HAS_TRANSCRIPT]- (artefact)
           OPTIONAL MATCH (person) -[:INTERVIEWED_AS]-> (speaker) -[:SAYS]-> (statement)
           WHERE speaker:Interviewee
           OPTIONAL MATCH (artefact) -[:HAS_ASSET]-> (asset)
-          RETURN statement, meta, person, speaker, asset, artefact, score
-          ORDER BY score DESC
-        `, {index, query})
+          RETURN statement, meta, person, speaker, asset, artefact
+        `, {query, token: OPENAI_API_KEY})
 
       const searchResults = result.records.map(record => {
         return {
@@ -32,7 +42,6 @@ export function useNeo4jTranscript(query: string, index: string): Neo4jResult[] 
           person: record.get('person'),
           artefact: record.get('artefact'),
           asset: record.get('asset'),
-          score: record.get('score'),
         }
       }).filter(
         result =>
@@ -44,7 +53,7 @@ export function useNeo4jTranscript(query: string, index: string): Neo4jResult[] 
     }
 
     void search()
-  }, [query, index, driver])
+  }, [query, driver])
 
   return searchResults
 }
--- packages/search-component/vite.config.js
+++ packages/search-component/vite.config.js
@@ -1,6 +1,11 @@
 import {nodeResolve} from '@rollup/plugin-node-resolve'
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   appType: 'spa',
@@ -13,6 +18,7 @@ export default defineConfig({
   ],
   publicDir: '../public',
   root: './src',
+  envDir: __dirname,
   server: {
     host: '0.0.0.0',
     port: 4040,
