c0548429925b7ff7772461836ff8032d3ea7d9d4
Daniel P. Shannon <daniel@try.works>
2025-09-20 20:36:07 -0400

    🚨 Delint monorepo root

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,43 @@
+# syntax=docker/dockerfile:1.7-labs
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Watchman Binaries ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 FROM phyllisstein/watchman:v2024.08.26.00 AS watchman
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ App ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-FROM ubuntu:22.04 AS app
+FROM ubuntu:24.04 AS app
 
 COPY --from=watchman /usr/local/bin/* /usr/local/bin/
 COPY --from=watchman /usr/local/lib/* /usr/local/lib/
 
-ENV NODE_MAJOR=22
+ENV NODE_MAJOR=24
 
 RUN apt-get update \
-  && DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" --allow-downgrades --allow-remove-essential --allow-change-held-packages --no-install-recommends \
-    ca-certificates \
-    curl \
-    git \
-    gnupg \
-  && mkdir -p /etc/apt/keyrings \
-  && curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key \
-    | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg \
-  && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list >/dev/null \
-  && curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null \
-  && echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list \
-  && apt-get update \
-  && DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" --allow-downgrades --allow-remove-essential --allow-change-held-packages \
-    nodejs \
-    yarn \
-  && mkdir -p /usr/local/var/run/watchman
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" --allow-downgrades --allow-remove-essential --allow-change-held-packages --no-install-recommends \
+        ca-certificates \
+        curl \
+        git \
+        gnupg \
+    && mkdir -p /etc/apt/keyrings \
+    && curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key \
+        | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg \
+    && echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list >/dev/null \
+    && curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null \
+    && echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list \
+    && apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" --allow-downgrades --allow-remove-essential --allow-change-held-packages \
+        nodejs \
+        yarn \
+    && mkdir -p /usr/local/var/run/watchman \
+    && chmod -R a+w /usr/local/var/run/watchman \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ENV PATH="/app/node_modules/.bin:/usr/share/nodejs/yarn/bin:$PATH"
 
 WORKDIR /app
 
-COPY scripts/develop.sh ./scripts/develop.sh
-COPY scripts/watchman ./scripts/watchman
+COPY --parents ./development ./
 
-RUN ./scripts/develop.sh watches
+ENTRYPOINT ["./scripts/develop.sh"]
 
-CMD ["./scripts/develop.sh", "watch"]
+CMD ["watch"]
--- eslint.config.js
+++ eslint.config.js
@@ -1,216 +1,260 @@
-import eslint from '@eslint/js'
-import stylistic from '@stylistic/eslint-plugin'
-import stylisticTS from '@stylistic/eslint-plugin-ts'
-import typescriptESLint from '@typescript-eslint/eslint-plugin'
-import parserTS from '@typescript-eslint/parser'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
-import react from 'eslint-plugin-react'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import sortKeysPlus from "eslint-plugin-sort-keys-plus";
+import stylistic from "@stylistic/eslint-plugin";
+import stylisticTS from "@stylistic/eslint-plugin-ts";
+import parserTS from "@typescript-eslint/parser";
+import tsSortKeys from "eslint-plugin-typescript-sort-keys";
+import tseslint from "typescript-eslint";
+import typescriptESLint from "@typescript-eslint/eslint-plugin";
+import sortDestructureKeys from "eslint-plugin-sort-destructure-keys";
+import eslint from "@eslint/js";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import next from "@next/eslint-plugin-next";
 
 export default [
-  {
-    ignores: ['**/dist', '**/node_modules', '**/public', '**/.next'],
-  },
-  eslint.configs.recommended,
-  stylistic.configs['recommended-flat'],
-  {
-    languageOptions: {
-      ecmaVersion: 2024,
-      globals: {
-        ...globals.browser,
-        ...globals.es2020,
-        ...globals.node,
-        ...globals.worker,
-      },
-      sourceType: 'module',
+    {
+        ignores: ["dist", "node_modules"],
     },
-    plugins: {
-      '@stylistic': stylistic,
-      '@stylistic/ts': stylisticTS,
-      'jsx-a11y': jsxA11y,
-      react,
+    eslint.configs.recommended,
+    stylistic.configs["recommended-flat"],
+    react.configs.flat.recommended,
+    react.configs.flat["jsx-runtime"],
+    {
+        languageOptions: {
+            ecmaVersion: 2024,
+            globals: {
+                ...globals.browser,
+                ...globals.es2020,
+                ...globals.node,
+                ...globals.worker,
+            },
+            sourceType: "module",
+        },
+        plugins: {
+            "@next/next": next,
+            "@stylistic": stylistic,
+            "jsx-a11y": jsxA11y,
+            react,
+            "react-hooks": reactHooks,
+            "sort-destructure-keys": sortDestructureKeys,
+            "sort-keys-plus": sortKeysPlus,
+        },
+        rules: {
+            ...next.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            ...jsxA11y.configs.recommended.rules,
+            "@stylistic/arrow-parens": [
+                "warn",
+                "as-needed",
+                {
+                    requireForBlockBody: false,
+                },
+            ],
+            "@stylistic/block-spacing": ["warn", "never"],
+            "@stylistic/brace-style": [
+                "warn",
+                "1tbs",
+                {
+                    allowSingleLine: true,
+                },
+            ],
+            "@stylistic/comma-dangle": [
+                "warn",
+                "always-multiline",
+            ],
+            "@stylistic/comma-spacing": [
+                "warn",
+                {
+                    after: true,
+                    before: false,
+                },
+            ],
+            "@stylistic/eol-last": "warn",
+            "@stylistic/indent": [
+                "warn",
+                4,
+            ],
+            "@stylistic/jsx-closing-bracket-location": ["warn", "after-props"],
+            "@stylistic/jsx-curly-newline": ["warn", "consistent"],
+            "@stylistic/jsx-curly-spacing": [
+                "warn",
+                {
+                    attributes: { when: "always" },
+                    children: { when: "always" },
+                    spacing: { objectLiterals: "never" },
+                    when: "always",
+                },
+            ],
+            "@stylistic/jsx-indent": [
+                "warn",
+                4,
+                {
+                    checkAttributes: true,
+                    indentLogicalExpressions: true,
+                },
+            ],
+            "@stylistic/jsx-indent-props": [
+                "warn",
+                4,
+            ],
+            "@stylistic/jsx-one-expression-per-line": ["warn", { allow: "single-line" }],
+            "@stylistic/jsx-quotes": [
+                "warn",
+                "prefer-double",
+            ],
+            "@stylistic/jsx-sort-props": [
+                "warn",
+                {
+                    callbacksLast: true,
+                    ignoreCase: true,
+                    noSortAlphabetically: false,
+                    reservedFirst: true,
+                    shorthandFirst: true,
+                },
+            ],
+            "@stylistic/jsx-tag-spacing": [
+                "warn",
+                {
+                    afterOpening: "never",
+                    beforeClosing: "never",
+                    beforeSelfClosing: "always",
+                    closingSlash: "never",
+                },
+            ],
+            "@stylistic/member-delimiter-style": [
+                "warn",
+                {
+                    multiline: {
+                        delimiter: "semi",
+                        requireLast: true,
+                    },
+                    singleline: {
+                        delimiter: "semi",
+                        requireLast: false,
+                    },
+                },
+            ],
+            "@stylistic/no-multiple-empty-lines": [
+                "warn",
+                {
+                    max: 2,
+                    maxBOF: 0,
+                    maxEOF: 1,
+                },
+            ],
+            "@stylistic/no-trailing-spaces": "warn",
+            "@stylistic/object-curly-spacing": [
+                "warn",
+                "always",
+            ],
+            "@stylistic/operator-linebreak": "warn",
+            "@stylistic/quote-props": ["warn", "consistent-as-needed"],
+            "@stylistic/quotes": [
+                "warn",
+                "double",
+                {
+                    allowTemplateLiterals: true,
+                    avoidEscape: true,
+                },
+            ],
+            "@stylistic/semi": [
+                "warn",
+                "always",
+                {
+                    omitLastInOneLineBlock: true,
+                    omitLastInOneLineClassBody: true,
+                },
+            ],
+            "@stylistic/space-before-function-paren": "warn",
+            "@stylistic/template-curly-spacing": ["warn", "always"],
+            "no-unused-vars": "warn",
+            "react/prop-types": "off",
+            "react-hooks/exhaustive-deps": [
+                "warn",
+                {
+                    additionalHooks: "(useRecoilCallback|useRecoilTransaction_UNSTABLE)",
+                },
+            ],
+            "sort-destructure-keys/sort-destructure-keys": [
+                "warn",
+                {
+                    caseSensitive: false,
+                },
+            ],
+            "sort-keys-plus/sort-keys": [
+                "warn",
+                "asc",
+                {
+                    caseSensitive: false,
+                    natural: true,
+                },
+            ],
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
     },
-    rules: {
-      '@stylistic/arrow-parens': [
-        'warn',
-        'as-needed',
-        {
-          requireForBlockBody: false,
+    {
+        files: [
+            "*.ts",
+            "*.tsx",
+            "**/*.ts",
+            "**/*.tsx",
+            "*.d.ts",
+            "**/*.d.ts",
+        ],
+        languageOptions: {
+            parser: parserTS,
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
-      ],
-      '@stylistic/block-spacing': 'off',
-      '@stylistic/ts/block-spacing': ['warn', 'always'],
-      '@stylistic/brace-style': [
-        'warn',
-        '1tbs',
-        {
-          allowSingleLine: true,
+        plugins: {
+            "@stylistic/ts": stylisticTS,
+            "@typescript-eslint": typescriptESLint,
+            "typescript-sort-keys": tsSortKeys,
         },
-      ],
-      '@stylistic/comma-dangle': [
-        'warn',
-        'always-multiline',
-      ],
-      '@stylistic/comma-spacing': [
-        'warn',
-        {
-          after: true,
-          before: false,
+        rules: {
+            ...tseslint.configs.recommendedTypeChecked.rules,
+            ...tseslint.configs.stylisticTypeChecked.rules,
+            "@stylistic/block-spacing": "off",
+            "@stylistic/object-curly-spacing": "off",
+            "@stylistic/ts/block-spacing": ["warn", "always"],
+            "@stylistic/ts/object-curly-spacing": [
+                "warn",
+                "always",
+            ],
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    args: "after-used",
+                    argsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                    varsIgnorePattern: "^_",
+                },
+            ],
+            "typescript-sort-keys/interface": [
+                "warn",
+                "asc",
+                {
+                    caseSensitive: false,
+                    natural: true,
+                },
+            ],
+            "typescript-sort-keys/string-enum": [
+                "warn",
+                "asc",
+                {
+                    caseSensitive: false,
+                    natural: true,
+                },
+            ],
         },
-      ],
-      '@stylistic/eol-last': 'warn',
-      '@stylistic/indent': [
-        'warn',
-        2,
-        {
-          ignoredNodes: [
-            'TSTypeParameterInstantiation',
-            'FunctionExpression > .params[decorators.length > 0]',
-            'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
-            'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key',
-          ],
-        },
-      ],
-      '@stylistic/jsx-closing-bracket-location': ['warn', 'after-props'],
-      '@stylistic/jsx-curly-newline': ['warn', 'consistent'],
-      '@stylistic/jsx-curly-spacing': [
-        'warn',
-        {
-          attributes: { when: 'always' },
-          children: { when: 'always' },
-          spacing: { objectLiterals: 'never' },
-          when: 'always',
-        },
-      ],
-      '@stylistic/jsx-indent': [
-        'warn',
-        2,
-        {
-          checkAttributes: true,
-          indentLogicalExpressions: true,
-        },
-      ],
-      '@stylistic/jsx-indent-props': [
-        'warn',
-        2,
-      ],
-      '@stylistic/jsx-one-expression-per-line': 'off',
-      '@stylistic/jsx-quotes': [
-        'warn',
-        'prefer-single',
-      ],
-      '@stylistic/jsx-sort-props': [
-        'warn',
-        {
-          callbacksLast: true,
-          ignoreCase: true,
-          noSortAlphabetically: true,
-          reservedFirst: true,
-          shorthandFirst: true,
-        },
-      ],
-      '@stylistic/jsx-tag-spacing': [
-        'warn',
-        {
-          afterOpening: 'never',
-          beforeClosing: 'never',
-          beforeSelfClosing: 'always',
-          closingSlash: 'never',
-        },
-      ],
-      '@stylistic/member-delimiter-style': [
-        'warn',
-        {
-          multiline: {
-            delimiter: 'none',
-          },
-          singleline: {
-            delimiter: 'comma',
-            requireLast: false,
-          },
-        },
-      ],
-      '@stylistic/no-multiple-empty-lines': [
-        'warn',
-        {
-          max: 2,
-          maxBOF: 0,
-          maxEOF: 1,
-        },
-      ],
-      '@stylistic/no-trailing-spaces': 'warn',
-      '@stylistic/object-curly-spacing': 'off',
-      '@stylistic/ts/object-curly-spacing': [
-        'warn',
-        'always',
-        { objectsInObjects: false },
-      ],
-      '@stylistic/operator-linebreak': 'warn',
-      '@stylistic/quote-props': ['warn', 'consistent-as-needed'],
-      '@stylistic/quotes': [
-        'warn',
-        'single',
-        {
-          allowTemplateLiterals: true,
-          avoidEscape: true,
-        },
-      ],
-      '@stylistic/semi': [
-        'warn',
-        'never',
-        {
-          beforeStatementContinuationChars: 'always',
-        },
-      ],
-      '@stylistic/space-before-function-paren': 'warn',
-      '@stylistic/template-curly-spacing': ['warn', 'always'],
-      'react/jsx-sort-props': [
-        'warn',
-        {
-          callbacksLast: true,
-          ignoreCase: true,
-          noSortAlphabetically: true,
-          reservedFirst: true,
-          shorthandFirst: true,
-          multiline: 'ignore',
-        },
-      ],
     },
-  },
-  {
-    files: [
-      '*.ts',
-      '*.tsx',
-      '**/*.ts',
-      '**/*.tsx',
-      '*.d.ts',
-      '**/*.d.ts',
-    ],
-    languageOptions: {
-      parser: parserTS,
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescriptESLint,
-    },
-    rules: {
-      ...tseslint.configs.recommendedTypeChecked.rules,
-      ...tseslint.configs.stylisticTypeChecked.rules,
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-          varsIgnorePattern: '^_',
-        },
-      ],
-      'no-undef': 'off',
-      'no-unused-vars': 'off',
-    },
-  },
-]
+];
