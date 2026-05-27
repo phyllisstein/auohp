11e73db1f41596861caa6981b52268a42d00248e
Daniel P. Shannon <daniel@try.works>
2026-05-27 10:59:09 -0400

    Unbreak Spectrum web components

--- packages/editor/src/routes/__root.tsx
+++ packages/editor/src/routes/__root.tsx
@@ -1,7 +1,5 @@
-import { createRootRoute, HeadContent, Outlet, ScrollRestoration, Scripts } from "@tanstack/react-router";
-import { useLayoutEffect } from "react";
-
-import { StyledComponentsRegistry } from "styles/global";
+import { createRootRoute, HeadContent, Outlet, Scripts } from "@tanstack/react-router";
+import { Body } from "styles/global";
 
 export const Route = createRootRoute({
     head: () => ({
@@ -14,31 +12,20 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
-    useLayoutEffect(() => {
-        import("@spectrum-web-components/theme/sp-theme.js");
-        import("@spectrum-web-components/theme/src/themes.js");
-        import("@spectrum-web-components/theme/theme-light.js");
-        import("@spectrum-web-components/theme/scale-large.js");
-        import("@spectrum-web-components/button/sp-button.js");
-        import("@spectrum-web-components/badge/sp-badge.js");
-    }, []);
     return (
-        <StyledComponentsRegistry>
-            <html className="spectrum spectrum--large spectrum--light" lang="en">
-                <head>
-                    <HeadContent />
-                </head>
-                <body>
-
-                    <sp-theme color="light" scale="large" system="spectrum">
-                        <main className="spectrum-Body spectrum-Body--sizeXL">
-                            <Outlet />
-                        </main>
-                    </sp-theme>
-                    <ScrollRestoration />
-                    <Scripts />
-                </body>
-            </html>
-        </StyledComponentsRegistry>
+        <html lang="en-US">
+            <head>
+                <HeadContent />
+            </head>
+            <body>
+                <Body />
+                <sp-theme color="light" scale="large" system="spectrum">
+                    <main className="spectrum-Body spectrum-Body--sizeXL">
+                        <Outlet />
+                    </main>
+                </sp-theme>
+                <Scripts />
+            </body>
+        </html>
     );
 }
--- packages/editor/src/routes/index.tsx
+++ packages/editor/src/routes/index.tsx
@@ -8,6 +8,15 @@ import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 
 export const Route = createFileRoute("/")({
     component: Page,
+    ssr: false,
+    beforeLoad: () => {
+        import("@spectrum-web-components/theme/sp-theme.js");
+        import("@spectrum-web-components/theme/src/themes.js");
+        import("@spectrum-web-components/theme/theme-light.js");
+        import("@spectrum-web-components/theme/scale-large.js");
+        import("@spectrum-web-components/button/sp-button.js");
+        import("@spectrum-web-components/badge/sp-badge.js");
+    },
 });
 
 const theme = {};
@@ -23,17 +32,21 @@ function Page() {
         theme,
     };
 
+
     return (
-        <LexicalComposer initialConfig={ initialConfig }>
-            <RichTextPlugin
-                contentEditable={ (
-                    <ContentEditable
-                        aria-placeholder="Enter some text..."
-                        placeholder={ <div>Enter some text...</div> } />
-                ) }
-                ErrorBoundary={ LexicalErrorBoundary } />
-            <HistoryPlugin />
-            <AutoFocusPlugin />
-        </LexicalComposer>
+        <>
+            <sp-button onClick={ () => console.log("Button clicked") }>Try me</sp-button>
+            <LexicalComposer initialConfig={ initialConfig }>
+                <RichTextPlugin
+                    contentEditable={ (
+                        <ContentEditable
+                            aria-placeholder="Enter some text..."
+                            placeholder={ <div>Enter some text...</div> } />
+                    ) }
+                    ErrorBoundary={ LexicalErrorBoundary } />
+                <HistoryPlugin />
+                <AutoFocusPlugin />
+            </LexicalComposer>
+        </>
     );
 }
