2bb3f0ef0db6952f7dbcbdfabbc1c64ecaa580e9
Daniel P. Shannon <daniel@try.works>
2026-07-23 14:40:33 -0400

    Fix issues with contiguous startTime/endTime

--- packages/editor/src/routes/interview.$interviewNumber.tsx
+++ packages/editor/src/routes/interview.$interviewNumber.tsx
@@ -5,9 +5,9 @@ import { Slate, Editable, type ReactEditor, withReact } from "slate-react";
 import { withHistory } from "slate-history";
 import { graphql } from "@/gql";
 import { useMutation, useReadQuery } from "@apollo/client/react";
-import { flow, debounce, throttle } from "es-toolkit/function";
+import { flow, debounce } from "es-toolkit/function";
 import { useVirtualizer } from "@tanstack/react-virtual";
-import { signal, effect, untracked } from "@preact/signals-react";
+import { signal, effect } from "@preact/signals-react";
 
 
 // FIXME: Constructing URLs for the caption endpoint and the public video URI
@@ -166,7 +166,7 @@ const TranscriptElement = ({ attributes, children, element }) => {
     }, []);
 
     effect(() => {
-        const index = element.children.findIndex(node => node.startTime <= playhead.value && playhead.value <= node.endTime);
+        const index = element.children.findIndex(node => node.startTime <= playhead.value && playhead.value < node.endTime);
 
         if (childVirtualizer.isScrolling || lastIndex.current === index) {
             return;
