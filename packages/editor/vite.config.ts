2961ac158027342dcce1b23a053ea84102d79faf
Daniel P. Shannon <daniel@try.works>
2026-08-20 23:46:43 -0400

    Clean up cargo-culted Vite/TanStack hacks

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vite/client" />
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react-swc";
+import viteReact from "@vitejs/plugin-react";
 import macros from "unplugin-parcel-macros";
 import optimizeLocales from "@react-aria/optimize-locales-plugin";
 import svgr from "vite-plugin-svgr";
 
 // See https://github.com/TanStack/router/discussions/6928#discussioncomment-16147477
-function withNormalizedMacroIds(plugin) {
+function withNormalizedMacroIds (plugin) {
     return {
         ...plugin,
         name: `${ plugin.name }-normalized-ids`,
-        transform(code, id) {
+        transform (code, id) {
             return plugin.transform?.call(this, code, id.replace(/\?.*$/, ""));
         },
     };
@@ -30,7 +30,7 @@ export default defineConfig({
         strictPort: true,
     },
     plugins: [
-        withNormalizedMacroIds(macros.vite()), // Must come first!
+        // withNormalizedMacroIds(macros.vite()), // Must come first!
         tanstackStart(),
         // viteReact must come after tanstackStart
         viteReact({
@@ -64,7 +64,7 @@ export default defineConfig({
                 // Bundle all S2 and style-macro generated CSS into a single bundle instead of code splitting.
                 // Because atomic CSS has so much overlap between components, loading all CSS up front results in
                 // smaller bundles instead of producing duplication between pages.
-                manualChunks(id) {
+                manualChunks (id) {
                     if (/macro-(.*)\.css$/.test(id) || /@react-spectrum\/s2\/.*\.css$/.test(id)) {
                         return "s2-styles";
                     }
@@ -79,5 +79,5 @@ export default defineConfig({
                 minify: false,
             },
         },
-    }
+    },
 });
