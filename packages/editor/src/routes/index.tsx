099171748c878d39ae2c7a946d0fb06c298a8825
Daniel P. Shannon <daniel@try.works>
2026-09-05 02:55:58 -0400

    Fix import of Search route

--- packages/editor/src/routes/index.tsx
+++ packages/editor/src/routes/index.tsx
@@ -5,7 +5,7 @@ import { useReadQuery } from "@apollo/client/react";
 import type { TypedDocumentNode } from "@apollo/client";
 import type { ListInterviewsQuery, ListInterviewsQueryVariables } from "./__generated__/index.gql";
 import { Route as InterviewRoute } from "./transcript/$interviewNumber";
-import { Route as SearchRoute } from "./transcript/search/route";
+import { Route as SearchRoute } from "./search/route";
 import { style, baseColor } from "@react-spectrum/s2/style" with { type: "macro" };
 import styled from "styled-components";
 
